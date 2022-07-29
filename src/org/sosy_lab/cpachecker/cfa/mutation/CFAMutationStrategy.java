2e77a731fa4b5af37326beba091b19462faf3fbd
Oleg Petrov <o.petrov@ispras.ru>
2022-07-29 18:31:50 +0300

    Add CFAMutationStrategy javadoc

--- src/org/sosy_lab/cpachecker/cfa/mutation/CFAMutationStrategy.java
+++ src/org/sosy_lab/cpachecker/cfa/mutation/CFAMutationStrategy.java
@@ -8,11 +8,17 @@
 
 package org.sosy_lab.cpachecker.cfa.mutation;
 
+/**
+ * A strategy capable to mutate a CFA in form of function CFAs (without interprocedure edges) with
+ * separate list of global declarations. Main function entry, language, and machine model are also
+ * supplied.
+ */
 interface CFAMutationStrategy {
 
   /**
    * Whether this strategy can mutate given CFA. Prepares strategy for next call to {@link #mutate}.
-   * Strategy either can mutate the CFA, or can return cause and safe parts of it.
+   * Strategy either can mutate the CFA, or can return cause and safe parts in it (all objects will
+   * fall in one of these categories).
    */
   public boolean canMutate(FunctionCFAsWithMetadata pCfa);
 
