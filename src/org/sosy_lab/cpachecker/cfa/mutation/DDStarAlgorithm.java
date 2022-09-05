300bde7f7ce47732554181237ba5066271c492ad
Oleg Petrov <o.petrov@ispras.ru>
2022-09-05 11:02:16 +0300

    Rename other two based on DD algorithms

--- src/org/sosy_lab/cpachecker/cfa/mutation/DDStarAlgorithm.java
+++ src/org/sosy_lab/cpachecker/cfa/mutation/DDStarAlgorithm.java
@@ -8,36 +8,65 @@
 
 package org.sosy_lab.cpachecker.cfa.mutation;
 
+import com.google.common.base.Preconditions;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import org.checkerframework.checker.nullness.qual.Nullable;
 import org.sosy_lab.common.log.LogManager;
+import org.sosy_lab.cpachecker.core.CPAcheckerResult.Result;
+import org.sosy_lab.cpachecker.core.defaults.MultiStatistics;
 import org.sosy_lab.cpachecker.core.interfaces.Statistics;
+import org.sosy_lab.cpachecker.core.reachedset.UnmodifiableReachedSet;
+import org.sosy_lab.cpachecker.util.statistics.StatCounter;
 
-/** // cause isolating // test minimizing via removal of deltas */
-class DeltaRemovingAfterCauseIsolatingDDStrategy<Element> implements CFAMutationStrategy {
-  private AbstractDeltaDebuggingAlgorithm<Element> delegate = null;
+/**
+ * A delta debugging algorithm that repeatedly finds minimal fail-inducing difference on remaining
+ * CFA. This way it finds a minimal failing test.
+ */
+class DDStarAlgorithm<Element> implements CFAMutationStrategy {
+  private DDAlgorithm<Element> delegate = null;
   private final LogManager logger;
   private final CFAElementManipulator<Element> elementManipulator;
   private List<Element> unresolvedElements = null;
   private List<Element> causeElements = new ArrayList<>();
-  private List<Statistics> stats = new ArrayList<>(2);
 
-  public DeltaRemovingAfterCauseIsolatingDDStrategy(
+  private final MultiStatistics stats;
+  private final StatCounter passes = new StatCounter("passes of DD cause isolation");
+
+  public DDStarAlgorithm(
       LogManager pLogger, CFAElementManipulator<Element> pElementManipulator) {
-    logger = pLogger;
-    elementManipulator = pElementManipulator;
+    logger = Preconditions.checkNotNull(pLogger);
+    elementManipulator = Preconditions.checkNotNull(pElementManipulator);
+
+    stats =
+        new MultiStatistics(logger) {
+          @Override
+          public void printStatistics(
+              PrintStream pOut, Result pResult, UnmodifiableReachedSet pReached) {
+            put(pOut, 0, passes);
+            super.printStatistics(pOut, pResult, pReached);
+          }
+
+          @Override
+          public @Nullable String getName() {
+            // TODO Auto-generated method stub
+            return "Cascade cause isolating DD on" + elementManipulator.getElementTitle();
+          }
+        };
   }
 
   @Override
   public void collectStatistics(Collection<Statistics> pStatsCollection) {
-    pStatsCollection.addAll(stats);
+    pStatsCollection.add(stats);
   }
 
   @Override
   public boolean canMutate(FunctionCFAsWithMetadata pCfa) {
-    if (delegate == null) {
+    if (unresolvedElements == null) {
       unresolvedElements = new ArrayList<>(elementManipulator.getAllElements(pCfa).nodes());
+      passes.inc();
       delegate =
           new DDAlgorithm<>(
               logger, elementManipulator, unresolvedElements);
@@ -46,17 +75,16 @@ class DeltaRemovingAfterCauseIsolatingDDStrategy<Element> implements CFAMutation
       // found cause
       causeElements.addAll(delegate.getCauseElements());
       unresolvedElements.retainAll(delegate.getSafeElements());
-      delegate.collectStatistics(stats);
+      delegate.collectStatistics(stats.getSubStatistics());
 
       if (unresolvedElements.isEmpty()) {
         return false;
       }
 
-      // we should reach here only once
-      assert delegate instanceof DDAlgorithm;
+      passes.inc();
       delegate =
-          new DDMinAlgorithm<>(
-              logger, elementManipulator, unresolvedElements, PartsToRemove.ONLY_DELTAS);
+          new DDAlgorithm<>(
+              logger, elementManipulator, unresolvedElements);
       boolean result = delegate.canMutate(pCfa);
       assert result;
     }
--- src/org/sosy_lab/cpachecker/cfa/mutation/DeltaRemovingAfterDDAlgorithm.java
+++ src/org/sosy_lab/cpachecker/cfa/mutation/DeltaRemovingAfterDDAlgorithm.java
@@ -8,61 +8,39 @@
 
 package org.sosy_lab.cpachecker.cfa.mutation;
 
-import com.google.common.base.Preconditions;
-import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import org.checkerframework.checker.nullness.qual.Nullable;
 import org.sosy_lab.common.log.LogManager;
-import org.sosy_lab.cpachecker.core.CPAcheckerResult.Result;
-import org.sosy_lab.cpachecker.core.defaults.MultiStatistics;
 import org.sosy_lab.cpachecker.core.interfaces.Statistics;
-import org.sosy_lab.cpachecker.core.reachedset.UnmodifiableReachedSet;
-import org.sosy_lab.cpachecker.util.statistics.StatCounter;
 
-class CascadeDeltaDebuggingCauseIsolatingStrategy<Element> implements CFAMutationStrategy {
-  private DDAlgorithm<Element> delegate = null;
+/**
+ * This delta debugging algorithm finds a minimal fail-inducing difference and then minimizes
+ * remaining 'safe' part by removing only deltas (usually DD tries to remove complements too).
+ */
+class DeltaRemovingAfterDDAlgorithm<Element> implements CFAMutationStrategy {
+  private AbstractDeltaDebuggingAlgorithm<Element> delegate = null;
   private final LogManager logger;
   private final CFAElementManipulator<Element> elementManipulator;
   private List<Element> unresolvedElements = null;
   private List<Element> causeElements = new ArrayList<>();
+  private List<Statistics> stats = new ArrayList<>(2);
 
-  private final MultiStatistics stats;
-  private final StatCounter passes = new StatCounter("passes of DD cause isolation");
-
-  public CascadeDeltaDebuggingCauseIsolatingStrategy(
+  public DeltaRemovingAfterDDAlgorithm(
       LogManager pLogger, CFAElementManipulator<Element> pElementManipulator) {
-    logger = Preconditions.checkNotNull(pLogger);
-    elementManipulator = Preconditions.checkNotNull(pElementManipulator);
-
-    stats =
-        new MultiStatistics(logger) {
-          @Override
-          public void printStatistics(
-              PrintStream pOut, Result pResult, UnmodifiableReachedSet pReached) {
-            put(pOut, 0, passes);
-            super.printStatistics(pOut, pResult, pReached);
-          }
-
-          @Override
-          public @Nullable String getName() {
-            // TODO Auto-generated method stub
-            return "Cascade cause isolating DD on" + elementManipulator.getElementTitle();
-          }
-        };
+    logger = pLogger;
+    elementManipulator = pElementManipulator;
   }
 
   @Override
   public void collectStatistics(Collection<Statistics> pStatsCollection) {
-    pStatsCollection.add(stats);
+    pStatsCollection.addAll(stats);
   }
 
   @Override
   public boolean canMutate(FunctionCFAsWithMetadata pCfa) {
-    if (unresolvedElements == null) {
+    if (delegate == null) {
       unresolvedElements = new ArrayList<>(elementManipulator.getAllElements(pCfa).nodes());
-      passes.inc();
       delegate =
           new DDAlgorithm<>(
               logger, elementManipulator, unresolvedElements);
@@ -71,16 +49,17 @@ class CascadeDeltaDebuggingCauseIsolatingStrategy<Element> implements CFAMutatio
       // found cause
       causeElements.addAll(delegate.getCauseElements());
       unresolvedElements.retainAll(delegate.getSafeElements());
-      delegate.collectStatistics(stats.getSubStatistics());
+      delegate.collectStatistics(stats);
 
       if (unresolvedElements.isEmpty()) {
         return false;
       }
 
-      passes.inc();
+      // we should reach here only once
+      assert delegate instanceof DDAlgorithm;
       delegate =
-          new DDAlgorithm<>(
-              logger, elementManipulator, unresolvedElements);
+          new DDMinAlgorithm<>(
+              logger, elementManipulator, unresolvedElements, PartsToRemove.ONLY_DELTAS);
       boolean result = delegate.canMutate(pCfa);
       assert result;
     }
